8a42c13144bd07e891f3c3e8f5ce06add355fc34
Nikolay Voronov <nkvoronov@gmail.com>
2019-08-08 16:26:56 +0300

    update 20_03

--- app/src/main/java/com/nkvoronov/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/nkvoronov/beatbox/BeatBoxFragment.java
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.nkvoronov.beatbox.databinding.FragmentBeatBoxBinding;
 import com.nkvoronov.beatbox.databinding.ListItemSoundBinding;
 
+import java.util.List;
+
 public class BeatBoxFragment extends Fragment {
     private BeatBox mBeatBox;
 
@@ -28,7 +30,7 @@ public class BeatBoxFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-        binding.recyclerView.setAdapter(new SoundAdapter());
+        binding.recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
         return binding.getRoot();
     }
 
@@ -41,19 +43,26 @@ public class BeatBoxFragment extends Fragment {
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
+
         @Override
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
-            ListItemSoundBinding binding = DataBindingUtil
-                    .inflate(inflater, R.layout.list_item_sound, parent, false);
+            ListItemSoundBinding binding = DataBindingUtil.inflate(inflater, R.layout.list_item_sound, parent, false);
             return new SoundHolder(binding);
         }
+
         @Override
         public void onBindViewHolder(SoundHolder holder, int position) {
         }
+
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 }
