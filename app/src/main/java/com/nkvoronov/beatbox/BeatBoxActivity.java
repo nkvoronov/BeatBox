4932e8950a6b259fab7b6787c3744daefc6760a0
Nikolay Voronov <nkvoronov@gmail.com>
2019-08-08 14:54:05 +0300

    init

---
+++ app/src/main/java/com/nkvoronov/beatbox/BeatBoxActivity.java
@@ -0,0 +1,13 @@
+package com.nkvoronov.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beatbox_activity);
+    }
+}
